58628bf6006b158527ec5c3f06d8b9fa941f49f0
Ian <ian.explore.world@gmail.com>
2017-06-12 19:04:19 +0900

    Add arrayToTree(), isSymmetric(), DFS, BFS in Tree

--- _Tree/src/Main.java
+++ _Tree/src/Main.java
@@ -1,27 +1,94 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Node root = new Node(7);
-		root.left = new Node(3);
-		root.right = new Node(9);
-		root.left.left = new Node(1);
-		root.left.right = new Node(5);
-		root.left.right.left = new Node(4);
-		
-		
-		
-		root.inOrder();
-		
+		/*
+		 * Inorder traverse String vs. StringBuilder
+		 */
+		{
+			final int NUM = 10000;
+			final int T6 = 1_000_000;
 
-		String result1 = root.inOrder("Str: ");
-		
-		StringBuilder sb = new StringBuilder("Sb:  ");
-		root.inOrderSB(sb);
-		
-		System.out.println();
-		System.out.println(result1);
-		System.out.println(sb.toString());
+			Integer[] arr1 = new Integer[NUM];
+			for (int i = 0; i < NUM; ++i) {
+				arr1[i] = i;
+			}
+			Node tree1 = Node.arrayToTree(arr1);
+
+			// In-order printout
+			tree1.printInOrder();
+			System.out.println();
+
+			// Concatenate with String
+			long ts1 = System.nanoTime();
+			String s = "Str: ";
+			s = tree1.inOrder(s); // slow
+			long te1 = System.nanoTime();
+
+			// Concatenate with StringBuilder
+			StringBuilder sb = new StringBuilder("Sb:  ");
+			long ts2 = System.nanoTime();
+			tree1.inOrder(sb); // fast
+			long te2 = System.nanoTime();
+
+			// System.out.println(s);
+			System.out.println("Inorder traversal of " + NUM + " units, String costs " + (te1 - ts1) / T6 + " ms");
+			// System.out.println(sb.toString());
+			System.out
+					.println("Inorder traversal of " + NUM + " units, StringBuilder costs " + (te2 - ts2) / T6 + " ms");
+		}
+		// ====================================================
+		/*
+		 * IsSymmetric function
+		 */
+		{
+			Node tree1 = Node.arrayToTree(new Integer[] { 1, 2, 2, 3, null, null, 3 });
+			Node tree2 = Node.arrayToTree(new Integer[] { 1, 2, 3, 3, null, 2, null });
+			System.out.println(Node.isSymmetric(tree1));
+			System.out.println(Node.isSymmetric(tree2));
+
+		}
+		// ====================================================
+		/*
+		 * BFS & DFS
+		 */
+		{
+			System.out.println("===== Tree1 =====");
+			Node tree1 = Node.arrayToTree(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17 });
+			System.out.println("** DFS(pre-order):");
+			tree1.printPreOrder();
+			System.out.println();
+
+			System.out.println("** DFS(in-order):");
+			tree1.printInOrder();
+			System.out.println();
+
+			System.out.println("** DFS(post-order):");
+			tree1.printPostOrder();
+			System.out.println();
+
+			System.out.println("** BFS:");
+			tree1.printBFS();
+			System.out.println();
+
+			System.out.println("===== Tree2 =====");
+			Node tree2 = Node
+					.arrayToTree(new Integer[] { 1, null, 2, null, null, 3, 4, null, null, null, null, 5, null, 6, 7 });
+			System.out.println("** DFS(pre-order):");
+			tree2.printPreOrder();
+			System.out.println();
+
+			System.out.println("** DFS(in-order):");
+			tree2.printInOrder();
+			System.out.println();
+
+			System.out.println("** DFS(post-order):");
+			tree2.printPostOrder();
+			System.out.println();
+
+			System.out.println("** BFS:");
+			tree2.printBFS();
+			System.out.println();
+		}
 	}
-	
 
 }
--- _Tree/src/Node.java
+++ _Tree/src/Node.java
@@ -1,24 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Node {
-	int id;
+	int val;
 	String name;
 	Node right, left;
 
+	Node() {
+	}
+
 	Node(int i) {
-		id = i;
+		val = i;
 		name = String.valueOf(i);
 	}
 
 	Node(int i, String n) {
-		id = i;
+		val = i;
 		name = n;
 	}
 
-	public void inOrder() {
+	public void printPreOrder() {
+		System.out.print(val + "-");
 		if (left != null)
-			left.inOrder();
-		System.out.print(id + " ");
+			left.printPreOrder();
 		if (right != null)
-			right.inOrder();
+			right.printPreOrder();
+	}
+
+	public void printInOrder() {
+		if (left != null)
+			left.printInOrder();
+		System.out.print(val + "-");
+		if (right != null)
+			right.printInOrder();
+	}
+
+	public void printPostOrder() {
+		if (left != null)
+			left.printPostOrder();
+		if (right != null)
+			right.printPostOrder();
+		System.out.print(val + "-");
 	}
 
 	public String inOrder(String string) {
@@ -29,12 +51,92 @@ class Node {
 			string = right.inOrder(string);
 		return string;
 	}
-	public void inOrderSB(StringBuilder string) {
+
+	// NO!!This method corresponding to StringBuilder's one cannot work with
+	// String.
+	// The reason is that String is immutable.
+	// public void inOrder(String ss) {
+	// if (left != null)
+	// left.inOrder(ss);
+	// ss += "-" + name;
+	// if (right != null)
+	// right.inOrder(ss);
+	// //return string.toString();
+	// }
+	public void inOrder(StringBuilder sb) {
 		if (left != null)
-			left.inOrderSB(string);
-		string.append("-" + name);
+			left.inOrder(sb);
+		sb.append("-" + name);
 		if (right != null)
-			right.inOrderSB(string);
-		//return string.toString();
+			right.inOrder(sb);
+		// return string.toString();
+	}
+
+	public static Node arrayToTree(Integer[] input) {
+		Node root = createNode(input, 1);
+		return root;
+	}
+
+	private static Node createNode(Integer[] input, int index) {
+		if (index <= input.length) {
+			Integer v = input[index - 1]; // tree's index starts from 1
+			if (v != null) {
+				Node t = new Node(v);
+				t.left = createNode(input, index * 2);
+				t.right = createNode(input, index * 2 + 1);
+				return t;
+			}
+		}
+		return null;
+	}
+
+	public static boolean isSymmetric(Node root) {
+		if (root == null)
+			return true;
+		if (root.left == null && root.right == null)
+			return true;
+		return isSymmetric(root.left, root.right);
+	}
+
+	private static boolean isSymmetric(Node left, Node right) {
+		if (left == null && right == null)
+			return true;
+		if (left == null || right == null)
+			return false;
+		if (left != null && right != null) {
+			if (left.val != right.val)
+				return false;
+			if (!isSymmetric(left.left, right.right))
+				return false; // outer
+			if (!isSymmetric(left.right, right.left))
+				return false; // inner
+		}
+		return true;
+	}
+
+	public void printBFS() {
+		List<Node> list = new ArrayList<Node>();
+		list.add(this);
+		while (!list.isEmpty()) {
+			Node current = list.remove(0);
+			System.out.print(current.val + "-");
+			if (current.left != null)
+				list.add(current.left);
+			if (current.right != null)
+				list.add(current.right);
+		}
+	}
+
+	public static void printBFS(Node n) {
+		List<Node> list = new ArrayList<Node>();
+		list.add(n);
+		while (!list.isEmpty()) {
+			Node current = list.remove(0);
+			System.out.print(current.val + "-");
+			if (current.left != null)
+				list.add(current.left);
+			if (current.right != null)
+				list.add(current.right);
+		}
 	}
 }
