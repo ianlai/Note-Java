ff5051456585457a48dcc81ae9e8f51e425d46b9
Ian <ian.explore.world@gmail.com>
2017-10-30 22:57:32 +0900

    Backtracking: Refactor Permutation

--- _Backtracking/src/permutation/Main.java
+++ _Backtracking/src/permutation/Main.java
@@ -2,36 +2,59 @@ package permutation;
 
 import java.util.List;
 
+import powerset._90_Subsets2;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Permutation p = new Permutation();
-		int[] arr = new int[] { 1, 2, 3 };
-		List<List<Integer>> result1 = p.permute1(arr);
-		List<List<Integer>> result2 = p.permute2(arr);
-		List<List<Integer>> result3 = p.permuteUnique(arr);
-
-		int[] arrRepeat = new int[] { 1, 2, 2, 3 };
-		List<List<Integer>> resultRepeat1 = p.permute1(arrRepeat);
-		List<List<Integer>> resultRepeat2 = p.permute2(arrRepeat);
-		List<List<Integer>> resultRepeat3 = p.permuteUnique(arrRepeat);
-
-		printList(result1); // correct
-		printList(result2); // correct
-		printList(result3); // correct
 		
-		printList(resultRepeat1); // wrong (redundant is also shown)
-		printList(resultRepeat2); // wrong (no result because the element will
-							      //        never be enough to be added in)
-		printList(resultRepeat3); // correct
+		int[] arr = new int[] { 1, 2, 3 };
+		int[] arrRepeat = new int[] { 1, 2, 2, 3 };
+		testPermutation1(arr);
+		testPermutation2(arrRepeat);
+		testSubset1(arr);
+		testSubset2(arrRepeat);
 
 	}
+	public static void testPermutation1(int arr[]){
+		_46_Permutations  p1 = new _46_Permutations();
+		_47_Permutations2 p2 = new _47_Permutations2();
+		List<List<Integer>> result1 = p1.permute1(arr);
+		//List<List<Integer>> result2 = p1.permute2(arr);
+		//List<List<Integer>> result3 = p2.permuteUnique(arr);
+		printList(result1); // correct
+		//printList(result2); // correct
+		//printList(result3); // correct
+	}
+	public static void testPermutation2(int arrRepeat[]){
+		_46_Permutations  p1 = new _46_Permutations();
+		_47_Permutations2 p2 = new _47_Permutations2();
+		//List<List<Integer>> resultRepeat1 = p1.permute1(arrRepeat);
+		//List<List<Integer>> resultRepeat2 = p1.permute2(arrRepeat);
+		List<List<Integer>> resultRepeat3 = p2.permuteUnique(arrRepeat);
 
+		//printList(resultRepeat1); // wrong (redundant is also shown)
+		//printList(resultRepeat2); // wrong (no result because the element will
+							      //        never be enough to be added in)
+		printList(resultRepeat3); // correct
+	}
+	public static void testSubset1(int arr[]){
+		_90_Subsets2 s2 = new _90_Subsets2();
+		List<List<Integer>> result = s2.subsets2(arr);
+		printList(result);
+	}
+	public static void testSubset2(int arr[]){
+		_90_Subsets2 s2 = new _90_Subsets2();
+		List<List<Integer>> result = s2.subsetsWithDup2(arr);
+		printList(result);
+	}
+	
+	
+	
 	public static void printList(List<List<Integer>> ll) {
 		for (int i = 0; i < ll.size(); ++i) {
 			System.out.println(i + 1 + ": " + ll.get(i));
 		}
 		System.out.println("------------------------");
 	}
-
 }
---
+++ _Backtracking/src/permutation/_46_Permutations.java
@@ -0,0 +1,73 @@
+package permutation;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class _46_Permutations {
+	boolean isDebug = true;
+
+	/*
+	 * Get all the permutations. Notice that all the input element should be
+	 * distinct. Use swap. 
+	 */
+	public List<List<Integer>> permute1(int[] arr) {
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		List<Integer> input = new ArrayList<Integer>();
+		if (arr.length == 0)
+			return result;
+		for (int e : arr) {
+			input.add(e);
+		}
+		_permutation(input, 0, result);
+		return result;
+	}
+
+	private void _permutation(List<Integer> arr, int k, List<List<Integer>> list) {
+		if (k == arr.size() - 1) {
+			List l = new ArrayList(arr);
+			// list.add(arr); //No, we need to create a new list to put into the
+			// double list
+			list.add(l);
+			return;
+		}
+		for (int i = k; i < arr.size(); ++i) {
+			Collections.swap(arr, i, k);
+			_permutation(arr, k + 1, list);
+			Collections.swap(arr, k, i);
+		}
+	}
+
+	// --------------------------------------------------
+	/*
+	 * Get all the permutations. Notice that all the input element should be
+	 * distinct. Use backtrack function (try).
+	 */
+	public List<List<Integer>> permute2(int[] nums) {
+		List<List<Integer>> list = new ArrayList<>();
+		// Arrays.sort(nums); // not necessary
+		backtrack(list, new ArrayList<>(), nums);
+		return list;
+	}
+
+	private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
+		if (tempList.size() == nums.length) {
+			list.add(new ArrayList<>(tempList));
+		} else {
+			for (int i = 0; i < nums.length; i++) {
+				if (tempList.contains(nums[i]))
+					continue; // element already exists, skip
+				tempList.add(nums[i]);
+				backtrack(list, tempList, nums);
+				tempList.remove(tempList.size() - 1);
+			}
+		}
+	}
+
+	// --------------------------------------------------
+	private void debug(String s) {
+		if (isDebug)
+			System.out.println(s);
+	}
+}
