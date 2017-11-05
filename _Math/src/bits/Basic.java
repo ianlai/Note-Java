574b64136f23aebcbd181cf1ca0ad11436dd64e1
Ian <ian.explore.world@gmail.com>
2017-11-05 18:50:48 +0900

    Bits: ReverseBits

--- _Math/src/bits/Basic.java
+++ _Math/src/bits/Basic.java
@@ -5,17 +5,19 @@ public class Basic {
 	public static void main(String[] args) {
 		Basic obj = new Basic();
 		obj.test_basic();
-		obj.test_countOne();
+		obj.test_shift();
+		//obj.test_countOne();
 	}
 	public void test_basic(){
 		int x1=  10;
 		int x2= -10;
 		
-		/* show */
+		/* show in binary form */
 		System.out.println(Integer.toBinaryString(x1));
 		System.out.println(Integer.toString(x1,2));
 		System.out.println(Integer.toString(x1,8));
 		
+		/* shift */
 		int y1 = x2 << 3;   //signed
 		int y2 = x2 >> 3;   //signed
 		int y3 = x2 >>> 3;  //unsigned right shift 
@@ -25,6 +27,28 @@ public class Basic {
 		System.out.println("y2: " + Integer.toString(y2,2));
 		System.out.println("y3: " + Integer.toString(y3,2));
 		
+		/* input binary form */
+		int b1 = 0b01001; 
+		System.out.println(b1);
+		System.out.println(Integer.toString(b1,2));
+	}
+	public void test_shift(){
+		int b1 = -0b1001110001100;
+		int t1 = b1; 
+		int t2 = b1; 
+		System.out.println("["+ t1 + "]");
+		while(t1!=0){
+			System.out.println(Integer.toBinaryString(t1));
+			t1>>>=1;
+		}
+		/* This will NOT stop (signed shift) */
+		/*
+		System.out.println("["+ t2 + "]");
+		while(t2!=0){
+			System.out.println(Integer.toBinaryString(t2));
+			t2>>=1;
+		}
+		*/
 	}
 	public void test_countOne(){
 		for(int i=0; i<20; i++){
--- _Math/src/bits/_190_ReverseBits.java
+++ _Math/src/bits/_190_ReverseBits.java
@@ -1,37 +1,70 @@
 package bits;
 
 public class _190_ReverseBits {
-    // you need treat n as an unsigned value
-//    public long reverseBits(long n) {
-//        String s = Long.toBinaryString(n);
-//        System.out.println(s);
-//        int size = s.length();
-//        StringBuilder sb = new StringBuilder();
-//        for(int i=0; i<size; ++i){
-//            sb.append(s.charAt(size-1-i));
-//        }
-//        String snew = sb.toString();
-//        System.out.println(snew);
-//        long result = Long.parseLong(snew, 2);
-//        return result;
-//    }
+	
+	/* Reverse bit of unsigned int */
 	
 	public static void main(String[] args) {
+		int n1 = 43261596;
+		int n2 = 1;
+		int n3 = 0x9000_0000;
 		
-		long l = 4294967295l;
-		int result = (new _190_ReverseBits()).reverseBits(123);
+		_190_ReverseBits obj = new _190_ReverseBits();
 		
+		obj.test(n1);
+		obj.test(n2);
+		obj.test(n3);
+		
+		System.out.println("=====================");
+		
+		obj.testSigned(n1);
+		obj.testSigned(n2);
+		obj.testSigned(n3);
+		
+		System.out.println("Conclusion: Both signed shift and unsigned shift can pass!");
+	}
+	public void test(int n){
+		int r = reverseBits(n);
+		
+		System.out.println("Input:  " + n); 
+		
+		System.out.print(String.format("%30s", Integer.toHexString(n))); 
+		System.out.print(" ---> ");
+		System.out.println(String.format("%10s", Integer.toHexString(r))); 
+		
+		System.out.println("Output: " + r); 
+		System.out.println();
+	}
+	public void testSigned(int n){
+		int r = reverseBits(n);
+		
+		System.out.println("Input:  " + n); 
+		
+		System.out.print(String.format("%30s", Integer.toHexString(n))); 
+		System.out.print(" ---> ");
+		System.out.println(String.format("%10s", Integer.toHexString(r))); 
+		
+		System.out.println("Output: " + r); 
+		System.out.println();
 	}
 	public int reverseBits(int n) {
-		System.out.println(n);
 	    int result = 0;
 	    for (int i = 0; i < 32; i++) {
-	        result += n & 1;
-	        n >>>= 1;   // CATCH: must do unsigned shift
+	        result |= n & 1;
 	        if (i < 31) // CATCH: for last digit, don't shift!
 	            result <<= 1;
+	        n >>>= 1;   // CATCH: must do unsigned shift
 	    }
-	    System.out.println(result);
+	    return result;
+	}
+	public int reverseBitsWrong(int n) {
+	    int result = 0;
+	    for (int i = 0; i < 32; i++) {
+	        result |= n & 1;
+	        if (i < 31) // CATCH: for last digit, don't shift!
+	            result <<= 1;
+	        n >>= 1;   // TEST: if use signed shift
+	    }
 	    return result;
 	}
     
