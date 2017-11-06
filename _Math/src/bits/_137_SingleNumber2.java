c787b6b26e76d3af304594ab5f537f7376a74a02
Ian <ian.explore.world@gmail.com>
2017-11-07 00:16:05 +0900

    Bits: SingleNumber2

---
+++ _Math/src/bits/_137_SingleNumber2.java
@@ -0,0 +1,43 @@
+package bits;
+
+/* Every element appears THREE times, except one only appears ONCE. Find it. */
+public class _137_SingleNumber2 {
+
+	public static void main(String[] args) {
+		_137_SingleNumber2 obj = new _137_SingleNumber2();
+		int[] arr1 = new int[]{1,1,1,3,3,3,7,7,7,8,8,66,8,4,4,4};  //66 once
+		int[] arr2 = new int[]{1,1,1,3,3,3,99,99,7,7,7,8,8,5,5,5,8,4,4,4}; //99 twice
+		
+		System.out.println(obj.singleNumber(arr1, true));   //correct
+		System.out.println(obj.singleNumber(arr1, false));  
+		
+		System.out.println(obj.singleNumber(arr2, true));   
+		System.out.println(obj.singleNumber(arr2, false));  //correct
+	}
+    public int singleNumber(int[] nums, boolean isOnce) {
+        
+    	/* Three 3 states need to be record, we use two bits. 
+    	 * 
+    	 * Define the state transition to be 
+    	 * AB: 00  -> 01  -> 10  -> 00
+    	 *     (0)    (1)    (2)    (3)
+    	 * Three input bits: A, B, input 
+    	 * Two output bits : NA, NB       //next state A, next state B 
+    	 * NA = ~ABI  | A~B~I
+    	 * NB = ~A~BI | ~AB~I = ~A(B^I) 
+    	 * 
+    	 * Because the special element will be at state-1 (once), return B (second).
+    	 * If the special element will be at state-2 (twice), then return A (first).
+    	 */
+        int first  = 0;
+        int second = 0;
+        for(int i=0; i<nums.length; i++){
+            int f = first;    //record
+            int s = second;   //not needed, just save the characters
+            int t = nums[i];  //not needed, just save the charaters
+            first  = ~f&s&t | f&~s&~t;   
+            second = ~f & (s^t); 
+        }
+        return isOnce ? second : first;  
+    }
+}
