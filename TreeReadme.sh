d04f4bc455aee1aad2719c9b30a9e00a6ec0ceed
Ian <ian.explore.world@gmail.com>
2017-11-08 22:23:00 +0900

    TreeReadme.sh supports OSX and Linux platforms

--- TreeReadme.sh
+++ TreeReadme.sh
@@ -3,13 +3,21 @@
 # Usage: ./TreeReadme.sh README.md
 # Note : This will DELETE the original file (README) 
 
+os="$(uname -s)"
+
 echo "## Project Tree" > $1
 echo "\`\`\`" >> $1     
 tree --noreport -I 'bin' >> $1
 echo "\`\`\`" >> $1
 #sed -i '1s/^/```/' $1  #Insert to the top (not necessary)
 
-quizNum=$(find . -regextype posix-extended -regex '^.+[_]{1}[0-9]{3}.+$' | nl | tail -1 | awk '{print $1}')
+### Mac OSX
+if [ $os == "Darwin" ]; then 
+    quizNum=$(find -E . -regex '^.+[_]{1}[0-9]{3}.+java$' | nl | tail -1 | awk '{print $1}')
+### Others (Linux) 
+else
+    quizNum=$(find . -regextype posix-extended -regex '^.+[_]{1}[0-9]{3}.+java$' | nl | tail -1 | awk '{print $1}')
+fi
 totalNum=$(find . -name '*.java' | nl | tail -1 | awk '{print $1}')
 
 echo "* Total Number: $totalNum" >> $1
