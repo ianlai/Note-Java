61996c9670d7733ad35cb373c407eea794280167
Ian <ian.explore.world@gmail.com>
2018-01-30 02:05:53 +0900

    Modify: 077, 047

--- _Backtracking/src/combinations/_077_Combinations.java
+++ _Backtracking/src/combinations/_077_Combinations.java
@@ -6,24 +6,32 @@ public class _077_Combinations {
 
 	public static void main(String[] args) {
 		_077_Combinations obj = new _077_Combinations();
-		List<List<Integer>> ll = obj.combine(4,3);
-		printList(ll);
+		List<List<Integer>> ll1 = obj.combine(4,3);
+		List<List<Integer>> ll2 = obj.combine(5,2);
+		printList(ll1);
+		printList(ll2);
 	}
 	public static void printList(List<List<Integer>> ll) {
 		for (int i = 0; i < ll.size(); ++i) {
-			System.out.println(i + 1 + ": " + ll.get(i));
+			System.out.println(String.format("%3s: %s", i, ll.get(i)));
 		}
 		System.out.println("------------------------");
 	}
+	
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> cur = new ArrayList<>();
         int[] nums = new int[n];
         boolean [] used = new boolean[n];
+        
+        /* Initialization (according to the quiz)
+         * Ex. n=4 -> [1,2,3,4]  
+         */
         for(int i=1; i<=n ;i++){
         	nums[i-1] = i;
         }
-        /* special case */
+        
+        /* Special case: Just copy the arr to result */
         if(n==k){
         	for(int e: nums){
         		cur.add(e);
@@ -31,28 +39,43 @@ public class _077_Combinations {
         	res.add(cur);
         	return res;
         }
-        /* normal case */
+        
+        /* Normal case: Backtracking is needed */
         backtracking(res, cur, nums, 0, k);
         //backtracking(res, cur, nums, used, k);
         return res;
     }
-    //private void backtracking(List<List<Integer>> ll, List<Integer> cur, int[] nums, boolean[] used, int left){
+    
+    /* We don't need a used array to record the whole array because combination*/ 
     private void backtracking(List<List<Integer>> ll, List<Integer> cur, int[] nums, int index, int left){
     	if(left==0){
     		ll.add(new ArrayList<Integer>(cur));
     	}else{
     		for(int i=index; i<nums.length; i++){
-    			//if(used[i]==true) continue;
     			cur.add(nums[i]);
-    			//used[i] = true;
-    			//backtracking(ll, cur, nums, index+nums[i], left-1);
-    			//backtracking(ll, cur, nums, index+1, left-1);
-    			backtracking(ll, cur, nums, nums[i], left-1);
+    			backtracking(ll, cur, nums, i+1, left-1);
     			                            //^ here is important
     			cur.remove(cur.size()-1);
-    			//used[i] = false;
     		}
     	}
     }
-
+    
+    /* backtracking function for #47 Permutation2 (for comparison) */
+	private void backtrackPermutation2(List<List<Integer>> list, List<Integer> tempList, int[] nums, boolean[] used) {
+		if (tempList.size() == nums.length) {
+			list.add(new ArrayList<>(tempList));
+		} else {
+			for (int i = 0; i < nums.length; i++) {
+				if (used[i])                                   //skip1: This is the normal permute 
+					continue;
+				if (i > 0 && nums[i]==nums[i-1] && !used[i-1]) //skip2: Handle duplicates 
+					continue;                                  
+				used[i] = true;
+				tempList.add(nums[i]);
+				backtrackPermutation2(list, tempList, nums, used);
+				used[i] = false;
+				tempList.remove(tempList.size() - 1);
+			}
+		}
+	}
 }
--- _Backtracking/src/permutations/_047_Permutations2.java
+++ _Backtracking/src/permutations/_047_Permutations2.java
@@ -7,12 +7,33 @@ import java.util.Collections;
 
 public class _047_Permutations2 {
 	boolean isDebug = true;
+	
+	public static void main(String[] args){
+		int[] arr = new int[] { 1, 2, 3, 4};
+		int[] arrRepeat = new int[] { 1, 2, 2, 3 };
+		_047_Permutations2 obj = new _047_Permutations2();
+		List<List<Integer>> result1 = obj.permuteUnique(arr);        //ok
+		List<List<Integer>> result2 = obj.permuteUnique(arrRepeat);  //ok
 
-	/* IMPORTANT */
+		printList(result1); // correct
+	    printList(result2); // correct
+	}
+
 	/*
 	 * Get all the permutations. Input element might have duplicates. 
 	 * Use backtrack function (try).
+	 * 
+	 * Remove the redundant: 
+	 * If it is a duplicated number, it can be used only when the previous duplicate is used! 
+	 * Don't put same value element at the same position.
+	 * 
+     * Ex.  1 2a 2b 2c 3
+     *      1 2b            x skip this because 2a (previous) is not used
+	 *      1 2a 2c         x skip this because 2a 2b pattern exists (2b not used)
+     *      1 2c            x skip this because 2b (previous) is not used
+     *      1 3 2a 2b 2c    o not skip this because 2b is in used, so they are different pattern  
 	 */
+ 
 	public List<List<Integer>> permuteUnique(int[] nums) {
 		List<List<Integer>> list = new ArrayList<>();
 		Arrays.sort(nums);
@@ -25,15 +46,10 @@ public class _047_Permutations2 {
 			list.add(new ArrayList<>(tempList));
 		} else {
 			for (int i = 0; i < nums.length; i++) {
-				if (used[i])                                         //skip1: This is the normal permute 
+				if (used[i])                                   //skip1: This is the normal permute 
 					continue;
-				if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) //skip2: Handle duplicates 
-					continue;                                        //       Don't put same value element at the same position
-					                                                 //       1 2a 2b 2c 3
-					                                                 //       1 2b            x skip this because 2a is not used
-					                                        		 //       1 2a 2c         x skip this because 2a 2b pattern exists (2b not used)
-				                                                     //       1 2c            x skip this because 2b==2c and 2b not used
-				                                                     //       1 3 2a 2b 2c    o not skip this because 2b is in used, so they are different pattern   
+				if (i > 0 && nums[i]==nums[i-1] && !used[i-1]) //skip2: Handle duplicates 
+					continue;                                  
 				used[i] = true;
 				tempList.add(nums[i]);
 				backtrack(list, tempList, nums, used);
@@ -47,5 +63,11 @@ public class _047_Permutations2 {
 	private void debug(String s) {
 		if (isDebug)
 			System.out.println(s);
+	}
+	public static void printList(List<List<Integer>> ll) {
+		for (int i = 0; i < ll.size(); ++i) {
+			System.out.println(String.format("%3s: %s", i+1, ll.get(i)));
+		}
+		System.out.println("------------------------");
 	}
 }
