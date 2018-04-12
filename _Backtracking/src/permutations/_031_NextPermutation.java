8f4d3c405f804ca7218c92f30d174732ce80172f
Ian <ian.explore.world@gmail.com>
2018-04-12 23:45:03 +0900

    QAKL: 013, 031

--- _Backtracking/src/permutations/_031_NextPermutation.java
+++ _Backtracking/src/permutations/_031_NextPermutation.java
@@ -2,7 +2,20 @@ package permutations;
 
 import java.util.Arrays;
 
+/*
+ * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * If such arrangement is not possible, it must rearrange it as the lowest possible order 
+ * (ie, sorted in ascending order).
+ * The replacement must be in-place, do not allocate extra memory.
+ * Here are some examples. 
+ * Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ */
+
 public class _031_NextPermutation {
+	boolean isDebug = true;
 
 	public static void main(String[] args) {
 		_031_NextPermutation obj = new _031_NextPermutation();
@@ -10,6 +23,7 @@ public class _031_NextPermutation {
 		int[] arr2 = new int[]{2,3,4,1};
 		int[] arr3 = new int[]{3,3,1,3,2};
 		int[] arr4 = new int[]{4,3,2,1};
+		int[] arr5 = new int[]{1,5,9,6,6,3,7,2,5,6,9,7,5,2};
 		
 		System.out.println(Arrays.toString(arr1));
 		obj.nextPermutation(arr1);
@@ -29,6 +43,12 @@ public class _031_NextPermutation {
 		System.out.println(Arrays.toString(arr4));
 		obj.nextPermutation(arr4);
 		System.out.println(Arrays.toString(arr4));
+		System.out.println("-------------");
+		
+		System.out.println(Arrays.toString(arr5));
+		obj.nextPermutation(arr5);
+		System.out.println(Arrays.toString(arr5));
+		System.out.println("-------------");
 		
 	}
     public void nextPermutation(int[] nums) {
@@ -41,15 +61,18 @@ public class _031_NextPermutation {
             if(nums[i-1] < nums[i]) 
                 lastUpIndex = i-1; 
         }
+        debug("lastUpIndex: " + lastUpIndex);
         
         /* Catch special case: 
-         * largest combination round up to smallest combination
+         * Largest combination round up to smallest combination
+         * Just reverse the array.
          * Ex. 321 -> 123
+         * 
          */
         if(lastUpIndex==-1){
             int[] temp = new int[l];
             for(int i=0; i<l; i++){
-                temp[l-i-1] = nums[i];
+                temp[l-i-1] = nums[i];  //reverse nums[]
             }
             for(int i=0; i<l; i++){
                 nums[i] = temp[i];
@@ -58,8 +81,22 @@ public class _031_NextPermutation {
         }
         
         /* Normal case:
-         * Ex. 123 -> 132
-         * Ex. 132 -> 213
+         * Ex. 123     -> 132
+         * Ex. 132     -> 213
+         * Ex. 1359872 -> 1372589  
+         *       -----      -----
+         *       5 is the last one increasing 
+         *       It means the subarray right after 5 is descending. [9872]
+         *       This also means it's the max of heading by 5. 
+         *       If we want to do a next operation of this, 5 should be swap with one larger than it.[7]
+         *       Then it becomes [7-9852]. 
+         *       This is the max of heading by 7. 
+         *       Because we need to find the min of heading by 7, we simply reverse the one after 7. 
+         *       In conclusion: 
+         *       (1) Find the last one increasing
+         *       (2) Find the min larger than (1) after (1)
+         *       (3) Swap (1) and (2)
+         *       (4) Reverse the subarray after (1)
          */
         lastUp = nums[lastUpIndex];
         int lastGreaterThanLastUp=-1;
@@ -70,16 +107,13 @@ public class _031_NextPermutation {
             if(nums[i] > lastUp) 
                 lastGreaterThanLastUpIndex = i;
         }
+        debug("lastGreaterThanLastUpIndex: " + lastGreaterThanLastUpIndex);
         lastGreaterThanLastUp = nums[lastGreaterThanLastUpIndex];
         
         /* Swap lastUp and lastGreaterThanLastUp */
         int swap = lastGreaterThanLastUp;
         nums[lastGreaterThanLastUpIndex] = lastUp;
         nums[lastUpIndex] = swap;
-        
-        //System.out.println("After Swap:" + Arrays.toString(nums));
-        //System.out.println("lastUp: " + lastUpIndex);
-        //System.out.println("lastGr: " + lastGreaterThanLastUpIndex);
         
         /* Reverse the remaining elements after lastUp */ 
         int tempSize = l-lastUpIndex-1;
@@ -90,6 +124,10 @@ public class _031_NextPermutation {
         for(int i=lastUpIndex+1; i<l; i++){
             nums[i] = temp[tempSize - 1 - i + (lastUpIndex+1)];
         }
-        //System.out.println("After Reverse:" + Arrays.toString(nums));
     }
+	// --------------------------------------------------
+	private void debug(String s) {
+		if (isDebug)
+			System.out.println(s);
+	}
 }
